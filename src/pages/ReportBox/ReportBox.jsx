6976c75ed3cf4524ef662b84c2aac789dc0bb011
0ccssu <kimsuaa0114@dgsw.hs.kr>
2024-10-28 20:41:27 +0900

    feat :: ReportBox 추가
    ReportBox 추가

---
+++ src/pages/ReportBox/ReportBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as C from "./ReportBoxStyle";
+
+const ReportBox = () => {
+  return (
+    <React.Fragment>
+        
+    </React.Fragment>
+  );
+};
+
+export default ReportBox;
